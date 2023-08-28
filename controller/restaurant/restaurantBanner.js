89f5d6f675f46a5565be1263064c548a72cc5b7a
rubiyadav <rubiflyeis@gmail.com>
2023-08-28 14:46:35 +0530

    create an api

--- controller/restaurant/restaurantBanner.js
+++ controller/restaurant/restaurantBanner.js
@@ -7,15 +7,15 @@ const mongoose = require('mongoose');
 exports.addBanner = async (req, res, next) => {
     try {
         console.log('hit admin add banner');
- 
-      //  if ( req.files.banner.length === 0) return next(createError(400, 'please provide the banner images'));
-       
-       // if (await Banner.findOne({})) return next(createError(400, 'a banner already exists please update the existing one'));
+
+        //  if ( req.files.banner.length === 0) return next(createError(400, 'please provide the banner images'));
+
+        // if (await Banner.findOne({})) return next(createError(400, 'a banner already exists please update the existing one'));
 
         // const pathArray = req.files.banner.map((item) => {
         //     return `${item.destination}/${item.filename}`
         // })
-      
+
         const newBanner = await Banner.create({
             images: /*pathArray*/req.body.images
         });
@@ -36,13 +36,13 @@ exports.addBanner = async (req, res, next) => {
 
 exports.updateBanner = async (req, res, next) => {
     try {
-             //  if ( req.files.banner.length === 0 ) return next(createError(400, 'please provide the banner images'));
+        //  if ( req.files.banner.length === 0 ) return next(createError(400, 'please provide the banner images'));
 
         // const pathArray = req.files.banner.map((item) => {
         //     return `${item.destination}/${item.filename}`
         // })
-      
-        const updated = await Banner.findOneAndUpdate({_id:req.params.id}, {
+
+        const updated = await Banner.findOneAndUpdate({ _id: req.params.id }, {
             images: /*pathArray*/req.body.images
         }, { new: true });
 
@@ -63,13 +63,13 @@ exports.getBanner = async (req, res, next) => {
     try {
         console.log('hit restaurant get banner');
 
-       // const banner = await Banner.findOne({});
-       const banner = await Banner.find();
-        if (!banner) return next(createError(400, 'cannot get the banner'));
+        // const banner = await Banner.findOne({});
+        const banner = await Banner.find();
+        if (banner.length == 0) return next(createError(400, 'cannot get the banner'));
 
         return res.status(200).json(banner);
 
-    } catch (error) { 
+    } catch (error) {
         console.log(error);
         return res.status(500).json({
             errorName: error.name,
@@ -81,23 +81,23 @@ exports.getBanner = async (req, res, next) => {
 
 exports.deleteBanner = async (req, res, next) => {
     try {
-       
-      const { id } = req.params;
-  
-      const deletedCoupon = await Banner.findOneAndDelete({
-       // restaurantId: req.user,
-        _id: id,
-      });
-  
-      if (!deletedCoupon)
-        return next(CreateError(400, "cannot delete the coupon"));
-  
-      return res.status(200).json({ message: " deleted successfully" });
+
+        const { id } = req.params;
+
+        const deletedCoupon = await Banner.findOneAndDelete({
+            // restaurantId: req.user,
+            _id: id,
+        });
+
+        if (!deletedCoupon)
+            return next(CreateError(400, "cannot delete the coupon"));
+
+        return res.status(200).json({ message: " deleted successfully" });
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        errorName: error.name,
-        message: error.message,
-      });
+        console.log(error);
+        return res.status(500).json({
+            errorName: error.name,
+            message: error.message,
+        });
     }
-  };
+};
