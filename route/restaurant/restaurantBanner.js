3de36e3d537688793b278ac4ec7d28f95867ab4a
rubiyadav <rubiflyeis@gmail.com>
2023-08-28 14:38:26 +0530

    create an api

--- route/restaurant/restaurantBanner.js
+++ route/restaurant/restaurantBanner.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const createError = require('http-errors');
 
-const { restaurantAuthMiddleware,adminAuthMiddleware ,userAuthMiddleware} = require('../../middleware/jwt');
-const { addBanner, getBanner, updateBanner,deleteBanner, } = require('../../controller/restaurant/restaurantBanner');
+const { restaurantAuthMiddleware, adminAuthMiddleware, userAuthMiddleware } = require('../../middleware/jwt');
+const { addBanner, getBanner, updateBanner, deleteBanner, } = require('../../controller/restaurant/restaurantBanner');
 const { getBannerByAdmin } = require('../../controller/admin/restaurantBanner');
 const { getBannerByUser } = require('../../controller/user/restaurantBanner');
 
@@ -11,7 +11,7 @@ const { getBannerByUser } = require('../../controller/user/restaurantBanner');
 //restaurant
 router.route('/banner').post(/*restaurantAuthMiddleware,/*cpUpload,*/addBanner).get(/*restaurantAuthMiddleware,*/getBanner)
 router.route('/adminbanner').get(/*adminAuthMiddleware,*/getBannerByAdmin)
-router.route('/userbanner').get(/*userAuthMiddleware,*/getBannerByUser)
+router.route('/user/banner').get(/*userAuthMiddleware,*/getBannerByUser)
 router.route('/updateBanner/:id').put(/*restaurantAuthMiddleware,/*cpUpload,*/updateBanner)
 router.route('/deleteBanner/:id').delete(deleteBanner)
 
